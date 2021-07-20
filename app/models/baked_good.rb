731d0582a0ff37955b334674e1095ceaea310e11
ihollander <ianholla@gmail.com>
2021-07-20 09:03:04 -0400

    Added solution

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,11 @@
 class BakedGood < ActiveRecord::Base
-  # add association macro here
+  belongs_to :bakery
+
+  # This method could also be written using a scope:
+  # https://guides.rubyonrails.org/active_record_querying.html#scopes
+  # scope :by_price, -> { order(price: :desc) }
+  def self.by_price
+    self.all.order(price: :desc)
+  end
+
 end
--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -1,3 +1,3 @@
 class Bakery < ActiveRecord::Base
-  # add association macro here
+  has_many :baked_goods
 end
